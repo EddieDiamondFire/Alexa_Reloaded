41ef3237f21c56a677051d32dc9dc06ea8fb717f
TheZachiePlayz123 <zhouedmund89@gmail.com>
2021-02-21 22:39:50 +1100

    Add new package and setup main.py bootstrap

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import discord
-import os
-import pathlib
-from discord.ext import commands
+import alexa
+import sys
+from common import utils as u
+
+# TODO add a debug option
+
+# Check current version of Speech Recognition
+u.print_version("Speech Recognition", alexa.check_version())
+
+if not(sys.version_info.major == 3 and sys.version_info.minor >= 9):
+    print("This python program requires to be running on 3.9 +")
+    print("Your current python version is " + str(sys.version_info.major) + "." + str(sys.version_info.minor))
+    exit(1)
 
 
-class Main:
-    client = commands.Bot(command_prefix='!bb')
-    bot_token = ""
+print("Starting Alexa")
+alexa.start_alexa_program()
+
+
+
+
 
 
